cc456c5cacb6730116a0e825758533d2d995e303
avihyb <avihyb@gmail.com>
2024-08-06 14:21:07 +0300

    new edit

--- src/app/projects/project-data.ts
+++ src/app/projects/project-data.ts
@@ -2,6 +2,27 @@ import { Project } from './project.model';
 
 export const PROJECTS: Project[] = [
     {
+        title: 'Opearting Systems Final Project',
+        description: 'MST in Graphs, Algorithms. Factory, Stratetgy, Pipeline (Active Object), Thread-Pool, Leader-Follower.',
+        image: 'images/mst.png',
+        url: 'https://github.com/avihyb/OS_Final_Project',
+        languages: ['C++']
+    },
+    {
+        title: 'My Angular Project',
+        description: 'Angular project which demonstrates the use of Angular components, services, and routing. The project also includes a custom Angular service that fetches data from a JSON file and displays it on the page. The project also includes a custom Angular component that displays a list of items and allows the user to add new items to the list. The project also includes a custom Angular component that displays a list of items and allows the user to delete items from the list. The project also includes a custom Angular component that displays a list of items and allows the user to edit items in the list.',
+        image: 'images/websiteIntro.png',
+        url: 'https://github.com/avihyb/avihyb.github.io',
+        languages: ['Angular', 'TypeScript', 'HTML', 'CSS']
+    },
+    {
+        title: 'Communication Networks Final Project',
+        description: 'QUIC Protocol: Python implemention of multiplexing.',
+        image: 'images/network.jpeg',
+        url: 'https://github.com/avihyb/final-project-network',
+        languages: ['Python']
+    },
+    {
         title: 'Viking Chess',
         description: 'A digital version of the ancient game Hnefatafl, also known as Viking Chess. Graphic User Interface (GUI) implemented using JavaFX. The game is played on a 11x11 board with two players: the attackers and the defenders. The attackers win by capturing the king, while the defenders win by moving the king to one of the four corner squares. The game features a custom board design, piece movement, and game logic. The game also includes a custom AI that can play against the player. The AI uses a minimax algorithm with alpha-beta pruning to determine the best move. The game also includes a custom game mode that allows the player to play against another player on the same computer',
         image: 'images/viking.png',
@@ -22,13 +43,8 @@ export const PROJECTS: Project[] = [
         url: 'https://github.com/avihyb/CPP-EX4',
         languages: ['C++']
     },
-    {
-        title: 'My Angular Project',
-        description: 'Angular project which demonstrates the use of Angular components, services, and routing. The project also includes a custom Angular service that fetches data from a JSON file and displays it on the page. The project also includes a custom Angular component that displays a list of items and allows the user to add new items to the list. The project also includes a custom Angular component that displays a list of items and allows the user to delete items from the list. The project also includes a custom Angular component that displays a list of items and allows the user to edit items in the list.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
-        url: 'https://github.com/avihyb/avihyb.github.io',
-        languages: ['Angular', 'TypeScript', 'HTML', 'CSS']
-    },
+    
+    
    
 
 ];
--- src/app/resume/timeline-data.ts
+++ src/app/resume/timeline-data.ts
@@ -1,5 +1,14 @@
 import { Timeline } from './timeline.model';
 
 export const TIMELINE_DATA: Timeline[] = [
-   {title: 'Computer Science Student', date: '2022 - Present', description: 'Studying Computer Science at a Ariels University. Learning about programming languages, algorithms, data structures, and software development principles.'},
+   {
+      title: 'BSc. Computer Science @ Ariel Univeristy',
+       date: '2022 - Present',
+        description: 'Currently studying Computer Science at Ariel University. Courses include: Data Structures, Algorithms, Operating Systems, Computer Networks, and more.',},
+
+        {
+         title: 'Saved for Position @ Compeny',
+         date: '2024 - Present',
+         description: 'TBU'
+        }
 ];
